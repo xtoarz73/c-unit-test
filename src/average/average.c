b0775924cfb16813cb6345b46508e32d4c490922
Alejandro Ramirez <aramirez@fbdfrozen.com>
2025-04-24 17:39:29 -0500

    Fixed average function for 0-length arrays

--- src/average/average.c
+++ src/average/average.c
@@ -1,6 +1,9 @@
 #include "average.h"
 
 float average(float arr[], unsigned int size){
+	if (size == 0){
+		return 0.0;
+	}		
 	float total = 0;
 	for(unsigned int i = 0; i < size ; i++){
 		total += arr[i];
